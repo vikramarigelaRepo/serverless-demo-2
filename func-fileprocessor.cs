e30b0312b6e79eb00637a3ee739f65bb9eb4f33a
Arigela, Naga Venkata Vikram <NagaVenkataVikram.Arigela@cognizant.com>
2020-12-30 16:48:51 +0530

    updated func

--- func-fileprocessor.cs
+++ func-fileprocessor.cs
@@ -11,7 +11,7 @@ namespace FileProcessor.Function
         [FunctionName("func_fileprocessor")]
         public static void Run([BlobTrigger("samples-workitems/{name}", Connection = "DestinationStorage")] Stream myBlob, string name, ILogger log)
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes \n CurrTime: {System.DateTime.Now}");
             // write code to validate headers and content.
         }
     }
