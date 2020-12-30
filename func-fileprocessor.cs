1d9ceec25692fc40417a11a1b960a8feeeda637b
Arigela, Naga Venkata Vikram <NagaVenkataVikram.Arigela@cognizant.com>
2020-12-30 16:24:20 +0530

    my first commit

---
+++ func-fileprocessor.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace FileProcessor.Function
+{
+    public static class func_fileprocessor
+    {
+        [FunctionName("func_fileprocessor")]
+        public static void Run([BlobTrigger("samples-workitems/{name}", Connection = "DestinationStorage")] Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+            // write code to validate headers and content.
+        }
+    }
+}
